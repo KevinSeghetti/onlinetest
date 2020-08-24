70b99220f076567ce96ea24d08781cb6fa7ca878
Kevin Seghetti <kts@localhost.localdomain>
2020-08-24 14:47:07 -0700

    all at once

--- src/App.js
+++ src/App.js
@@ -1,24 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
+import PropTypes from 'prop-types'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import FormControl from 'react-bootstrap/FormControl'
+import InputGroup from 'react-bootstrap/InputGroup'
 import './App.css';
 
+const DisplayImageList = ({imageData}) =>
+{
+    if(imageData.data)
+    {
+        const rows = imageData.data.map( (entry,index) =>
+        {
+            //let url =<img src="https://media3.giphy.com/media/tJU72w9lPzUPe/200.gif?cid=ecf05e47zjcyo52jtpwog4ifxxcho8fjlcznike8sutufmsw&rid=200.gif" alt="react" className="logo"/>
+            let image = entry.images.fixed_width_small
+            return <Col key={entry.id} className="Trending">
+                    <img src={image.url} width={image.width} height={image.height} alt={entry.title} />
+                </Col>
+        }
+        )
+
+        return (
+            <div>
+                <Row>
+                    {rows }
+                </Row>
+        </div>
+        )
+    }
+    return "<h2>Loading<h2>"
+
+}
+
+DisplayImageList.propTypes = {
+    imageData: PropTypes.object.isRequired,
+}
+
+// <img src="https://media3.giphy.com/media/tJU72w9lPzUPe/200.gif?cid=ecf05e47zjcyo52jtpwog4ifxxcho8fjlcznike8sutufmsw&rid=200.gif" alt="react" className="logo"/>
+const api_key = '5Muqe6HOngq40S9xI6ZQJ7jDfvZUoS5f'
+const trendingUrl = 'http://api.giphy.com/v1/gifs/trending'
+
+const InitialLoad = (urlRoot,setTrendingData) =>
+{
+
+    let url = `${urlRoot}?api_key=${api_key}&limit=25`
+    console.log(url)
+
+    fetch(`${trendingUrl}?api_key=${api_key}`
+   )
+  .then(response => response.json())
+  .then(data =>
+    {
+        console.log(data)
+        setTrendingData(data)
+
+    });
+}
+
 function App() {
+
+  let [trendingData,setTrendingData] = useState({})
+  let [searchData,setSearchData] = useState({})
+
+  if(!trendingData.data)
+  {
+      InitialLoad(trendingUrl,setTrendingData)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h2>Trending Gifs!!</h2>
+          <DisplayImageList imageData={trendingData}/>
+          <InputGroup className="mb-3">
+             <InputGroup.Prepend>
+               <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+             </InputGroup.Prepend>
+             <FormControl
+               placeholder="Search String"
+               aria-label="Search string"
+              onEnter= {
+                  console.log("###")
+              }
+             />
+          </InputGroup>
+              <h2>Search Results</h2>
+
+          <DisplayImageList imageData={searchData}/>
+
+          <br />
+
     </div>
   );
 }
